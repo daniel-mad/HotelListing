914682420237e57516f061d8362613860d385408
Daniel Madhala <danielmad87@gmail.com>
2022-08-25 13:22:08 +0300

    RESR API Development best practices.
    AutoMapper and Repositories

---
+++ HotelListing.API/Models/Country/GetCountryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelListing.API.Models.Country
+{
+    public class GetCountryDto : BaseCountryDto
+    {
+        public int Id { get; set; }
+
+    }
+
+   
+}
---
+++ HotelListing.API/Models/Country/UpdateCountryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace HotelListing.API.Models.Country
+{
+    public class UpdateCountryDto : BaseCountryDto
+    {
+        public int Id { get; set; }
+
+    }
+
+   
+}
